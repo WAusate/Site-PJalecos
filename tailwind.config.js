b02d015776e9540b7fb8245bf071a5aa791573f5
WAusate <wausate@gmail.com>
2025-10-02 17:07:16 -0300

    Apply Personalize Jalecos color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          primary: "#74B3A4",
+          secondary: "#2E2E2E",
+          background: "#FFFFFF",
+          complementary: "#E0E0E0",
+          accent: "#4C8C82",
+        },
+      },
+    },
   },
   plugins: [],
 }
